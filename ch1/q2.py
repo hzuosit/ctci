1c1a3f7c368409960a77c4e44f19817184113310
hao zuo <haozuo@haos-air.home>
2016-03-21 07:52:22 -0400

    some change

--- ch1/q2.py
+++ ch1/q2.py
@@ -1,18 +1,27 @@
 #Given two string, check permutation 
-
 #Sort and compare solution
 class check:
-		def checkPermu(s1,s2):
-				if sorted(s1) == sorted(s2):
-						print s1+' is permutation of '+s2
-				else:
-						print s1 +' is not a permutation of '+s2
-        
+	def checkPermu(self,s1,s2):
+		if sorted(s1) == sorted(s2):
+			print s1+' is permutation of '+s2
+		else:
+			print s1 +' is not a permutation of '+s2
+# use dictionary to check the permutation
+	def permuDic(self,s1,s2):
+		d = {}
+		for w in s1:
+			if d[w] != 0:
+				d[w]+=1
+			else:
+
+		
+
 def main():
 		s1 = 'abcdefg'
 		s2 = 'fgcdbae'
 		s3 = 'sadfasdfasdfa'
 		c = check()
 		c.checkPermu(s1,s2)
+		c.checkPermu(s2,s3)
 if __name__=='__main__':
 		main()
