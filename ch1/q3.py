9f809b657264dd17205568f50f23500ac3865c35
hao zuo <haozuo@haos-air.home>
2016-03-23 16:47:08 -0400

    add q5

--- ch1/q3.py
+++ ch1/q3.py
@@ -1,6 +1,26 @@
-#urlify  
+#urlify 
+# first method use insertion, however, the insert() time complexity is O(n),thus the for loop here will consume O(n*n), it's low efficiency.
 def urlify(s):
-	a = s.split('')
+	a = s.split()
+	l = 2*len(a)-1
+	for x in xrange(1,l,2):
+		a.insert(x,'%20')
+	print  ''.join(a)
+
+#this is best way 
+def urlify2(s):
+	print  '%020'.join(s.split())
+
+def main():
+	s = 'asdf asdf awer add'
+	urlify(s)
+	k = 'qwe wl di siq so'
+	urlify2(k)
+if __name__ == "__main__":
+	main()
+
+	
+
 
 
 
---
+++ ch1/q5.py
@@ -0,0 +1,35 @@
+#One Away
+def oneAway(s1,s2):
+	if len(s1)<len(s2):
+		s1,s2 = s2,s1
+	if abs(len(s1)-len(s2))>1:
+		return False
+	else:
+		i,j = 0,0
+	        check = False
+	        while i<len(s1) and j<len(s2):
+			if s1[i] != s2[j]:
+				if check:
+					return False
+			        check = True
+			        if len(s1) == len(s2):
+					j += 1
+			        i+=1
+		        else:
+				i+=1
+			        j+=1
+	return True
+
+def main():
+	s1 = 'pale'
+	s2= 'bale'
+	s3 = 'pales'
+	s4 = 'palesss'
+	a=oneAway(s2,s1)
+	b=oneAway(s1,s3)
+	c=oneAway(s2,s3)
+	d = oneAway(s1,s4)
+	print a,b,c,d
+
+if __name__ == '__main__':
+	main()
