7aa6ec7f2cedec83a8785b0a879c9300d6dd3527
Zhow <zhow@zh.local>
2016-04-04 15:13:34 -0400

    add q2

--- ch2/q2.py
+++ ch2/q2.py
@@ -4,7 +4,24 @@ from linked_list import *
 def k_tolast(k,ll):
 	node = ll.head
 	s = ll.size
-	
+	for i in range(s-k):
+		if node is not None:
+			node = node.next
+		else:
+			return
+	return node
+
+
+def main():
+	k = 5
+	ll = random_list(20)
+	print ll
+	print  k_tolast(k,ll)
+
+
+if __name__ == '__main__':
+	main()
+
 
 	
 	
